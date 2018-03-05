38380038086b809310ca536e6e8d76a891711b08
CreativeFingers <30295395+CreativeFingers@users.noreply.github.com>
2018-03-05 11:39:42 +0530

    aading javascript
    changes

--- style.js
+++ style.js
@@ -49,14 +49,153 @@ $(".continue_part").click(function(){
 	
 });//hide shoe end
 
+
+
+
+
 $("#occupationSelect").change(function(){
-	$("#occupationHide").hide();
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue){
+                $(".subDiv").not("." + optionValue).hide();
+                $("." + optionValue).show();
+            } else{
+                $(".subDiv").hide();
+            }
+        });
+    }).change();
 	
-	$("#")+.this().val().show()
-});
+	
+	// dental field category change
+	
+	$("#dentalFieldCategory").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "hygienist"){
+                $(".hygiene_exam").show();
+                
+            } else{
+                $(".hygiene_exam").hide();
+            }
+        });
+    }).change();
+		// FULLTIME change
+	
+	$("#dentalFieldFulltime").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "fulltime_val"){
+                $(".fullTime").show();
+                
+            } else{
+                $(".fullTime").hide();
+            }
+        });
+    }).change();
+			// therapist sub div change
+	
+	$("#therapistSubPlease").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "please_val"){
+                $(".therapist_sub").show();
+                
+            } else{
+                $(".therapist_sub").hide();
+            }
+        });
+    }).change();
+	//diet part
+	
+	$("#otherDiet").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "Nutrition_val"){
+                $(".dietSub").show();
+                
+            } else{
+                $(".dietSub").hide();
+            }
+        });
+    }).change();
+	
+	//hospital part
+	
+	$("#hospital_administator").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "hospitalSub"){
+                $(".hospiSub").show();
+                
+            } else{
+                $(".hospiSub").hide();
+            }
+        });
+    }).change();
+	//administator_sub part
+	
+	$("#administator_sub").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "administratorSub"){
+                $(".administrator_ans").show();
+                
+            } else{
+                $(".administrator_ans").hide();
+            }
+        });
+    }).change();
+	
+		
+	$("#administator_sub").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "otherMedical"){
+                $(".otherMedical_ans").show();
+                
+            } else{
+                $(".otherMedical_ans").hide();
+            }
+        });
+    }).change();
+	//health plan
+		$("#healthPlan").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "healthPlan_val"){
+                $(".helthy_many").show();
+                
+            } else{
+                $(".helthy_many").hide();
+            }
+        });
+    }).change();
+	
+	$("#healthPlan").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "healthDescribeVal"){
+                $(".healthDescribe").show();
+                
+            } else{
+                $(".healthDescribe").hide();
+            }
+        });
+    }).change();
+	//pharma
+	$("#fulltimePharma").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue === "fulltimePharma_val"){
+                $(".fullPharma").show();
+                
+            } else{
+                $(".fullPharma").hide();
+            }
+        });
+    }).change();
 
-
-
+	
 
 
 
